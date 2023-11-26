3d6a105c29b015ff8d2907ab97c0ba207597d394
Valaphee <32491319+valaphee@users.noreply.github.com>
2023-11-26 16:18:25 +0100

    Initial commit

---
+++ src/main/java/com/valaphee/soundBlock/ModClass.java
@@ -0,0 +1,31 @@
+package com.valaphee.soundBlock;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ModClass.MODID, name = ModClass.NAME, version = ModClass.VERSION)
+public class ModClass
+{
+    public static final String MODID = "valapheesoundblock";
+    public static final String NAME = "Valaphee's Sound Block";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
