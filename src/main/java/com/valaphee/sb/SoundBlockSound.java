df40fdfee22b7fca2132e85f149407f9e83a9e72
Cre8or <cre8or@tuta.io>
2023-12-31 16:18:32 +0100

    Prevent accidental jump scares
    - Prevent sounds from accidentally blowing one's ear drums out on their first tick

--- src/main/java/com/valaphee/sb/SoundBlockSound.java
+++ src/main/java/com/valaphee/sb/SoundBlockSound.java
@@ -15,9 +15,13 @@ public class SoundBlockSound extends MovingSound {
         this.sound = sound;
         this.repeat = looping;
 
-        this.xPosF = (float) ((double) blockData.getPos().getX() + blockData.getOffsetX() + sound.getOffsetX());
-        this.yPosF = (float) ((double) blockData.getPos().getY() + blockData.getOffsetY() + sound.getOffsetY());
-        this.zPosF = (float) ((double) blockData.getPos().getZ() + blockData.getOffsetZ() + sound.getOffsetZ());
+        // Since we're achieving distance via initially high volumes, we need to manually
+        // prevent "accidental jump scares" on the first sound tick. Your heart (and ears)
+        // will thank me later!
+        this.xPosF = 0.0f;
+        this.yPosF = Float.POSITIVE_INFINITY;
+        this.zPosF = 0.0f;
+
         this.volume = blockData.getDistance() * sound.getDistance();
         this.pitch = blockData.getPitch() * sound.getPitch();
     }
